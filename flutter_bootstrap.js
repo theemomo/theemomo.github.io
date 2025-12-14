04083b17b970e43b0c6724e5cdf386f96a3b72bd
Mohammad Moustafa <mohammad.moustafa.m1@gmail.com>
2025-12-14 21:50:45 +0200

    fix downloading error

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1868013963"
+    serviceWorkerVersion: "2741989296"
   }
 });
